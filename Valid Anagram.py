c3c538cf81a01d21a5a94bf003242f981db9665c
Tushar K Rao <tusharkrao999@gmail.com>
2023-12-18 00:15:54 -0700

    Valid Anagram

---
+++ Valid Anagram.py
@@ -0,0 +1,11 @@
+def isAnagram(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    if ''.join(sorted(s)) == ''.join(sorted(t)):
+        return True
+    else:
+        return False
+
