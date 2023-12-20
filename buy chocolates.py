0b252956f96a6738ca67a38e63f7741c283e000c
Tushar K Rao <tusharkrao999@gmail.com>
2023-12-20 14:07:10 -0700

    buy chocolates

---
+++ buy chocolates.py
@@ -0,0 +1,11 @@
+def buyChoco(self, prices, money):
+    """
+    :type prices: List[int]
+    :type money: int
+    :rtype: int
+    """
+    prices.sort()
+    if prices[0] + prices[1] <= money:
+        return money - (prices[0] + prices[1])
+    else:
+        return money
