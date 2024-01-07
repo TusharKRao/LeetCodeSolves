34e6f6f06216810b6e86b75913482c321b629d85
Tushar K Rao <tusharkrao999@gmail.com>
2024-01-06 19:46:31 -0700

    grid traveller tabulated

--- gridTraveller.py
+++ gridTraveller.py
@@ -12,3 +12,28 @@ def gridTravel (m, n, memo={}):
     return memo[key]
 
 print(gridTravel(18,18))
+
+
+#tabulated
+
+def gd(m, n):
+    t = [[0] * (n+1) for j in range(m+1)]
+    for i in range(0, m+1):
+        for j in range(0, n+1):
+            if i == 0 or j == 0:
+                t[i][j] = 0
+    t[1][1] = 1
+    #initialization complete
+
+    for i in range(0, m+1):
+
+        for j in range(0, n+1):
+
+            if i+1 <= m:
+                t[i+1][j] += t[i][j]
+            if j+1 <= n:
+                t[i][j+1] += t[i][j]
+
+    print(t[m][n])
+
+gd(18,18)
--- tester.py
+++ tester.py
@@ -1,18 +1,37 @@
-def howSum (s, nums, memo = {}):
-    if s in memo:
-        return s
-    if s == 0:
-        return []
-    if s < 0:
-        return None
+def gridTravel (m, n, memo={}):
+    key = (m, n)
+    if key in memo:
+        return memo[key]
+    if m == 0 or n == 0:
+        memo[key] = 0
+        return 0
+    if m == 1 and n == 1:
+        memo[key] = 1
+        return 1
+    memo[key] = gridTravel(m-1, n, memo) + gridTravel(m, n-1, memo)
+    return memo[key]
 
-    for num in nums:
-        rem = s - num
-        remres = howSum(rem, nums, memo)
-        if remres is not None:
-            remres += [num]
-            memo[rem] = remres
-            return remres
-    return None
+print(gridTravel(18,18))
 
-print(howSum(1000,[2, 3, 5]))
+
+def gd(m, n):
+    t = [[0] * (n+1) for j in range(m+1)]
+    for i in range(0, m+1):
+        for j in range(0, n+1):
+            if i == 0 or j == 0:
+                t[i][j] = 0
+    t[1][1] = 1
+    #initialization complete
+
+    for i in range(0, m+1):
+
+        for j in range(0, n+1):
+
+            if i+1 <= m:
+                t[i+1][j] += t[i][j]
+            if j+1 <= n:
+                t[i][j+1] += t[i][j]
+
+    print(t[m][n])
+
+gd(18,18)
