c31cd23f4a2067afa40460c77a84ce8f6f75a0cf
Tushar K Rao <tusharkrao999@gmail.com>
2024-01-13 17:31:36 -0700

    543. diameter of binary tree

---
+++ diameter of binary tree.py
@@ -0,0 +1,12 @@
+def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    ans=0
+    def dia(root):
+        nonlocal ans
+        if not root:
+            return 0
+        left=dia(root.left)
+        right=dia(root.right)
+        ans=max(ans,left+right)
+        return 1+max(right,left)
+    dia(root)
+    return ans
