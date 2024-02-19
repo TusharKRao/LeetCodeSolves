c8af2c8f27b9649bd30b34b3b31d0bf312459adc
Tushar K Rao <tusharkrao999@gmail.com>
2024-02-19 15:11:51 -0700

    1642: furthest building you can reach

--- tester.py
+++ tester.py
@@ -1,26 +1,6 @@
-
-import math
-def numSquares(n):
-    """
-    :type n: int
-    :rtype: int
-    """
-    dp = [None] * (n+1)
-    dp[0] = 0
-    mex = int(math.sqrt(n))
-    pool = []
-    for i in range(1, mex+1):
-        pool.append(i*i)
-    while dp[n] is None:
-        for i, item in enumerate(dp):
-
-            if item is not None:
-                for sq in pool:
-                    nu = sq + i
-                    if nu <= n and dp[nu] is not None:
-                        dp[nu] = min(dp[nu], item + 1)
-                    elif nu <= n:
-                        dp[nu] = item + 1
-    return dp[n]
-
-print(numSquares(12))
+strong = "this is a demo string where im trying the slice application"
+x= strong[::2]
+x2 =strong[1::2]
+print(strong)
+print(x)
+print(x2)
